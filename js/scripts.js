4f45771056f874497f2e4909a97407226d1ca4f5
Emily Watkins and Lieghti Sharp <emily@>
2017-11-15 20:01:58 -0800

    add the address type to the js.

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,11 @@ function Contact(first, last) {
   this.address = [];
 }
 
-function Address(street, city, state) {
+function Address(street, city, state, type) {
   this.street = street;
   this.city = city;
   this.state = state;
+  this.type = type;
 }
 
 Contact.prototype.fullName = function() {
@@ -16,7 +17,16 @@ Contact.prototype.fullName = function() {
 }
 
 Address.prototype.fullAddress = function() {
-  return this.street + ", " + this.city + " " + this.state;
+  return this.street + ", " + this.city + " " + this.state + ", " + this.type;
+}
+
+function resetFields() {
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.new-street").val("");
+  $("input.new-city").val("");
+  $("input.new-state").val("");
+  $("input.new-type").val("");
 }
 
 $(document).ready(function() {
@@ -31,7 +41,8 @@ $(document).ready(function() {
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      var inputtedType = $(this).find("input.new-type").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedType);
       newContact.address.push(newAddress);
     });
 
@@ -48,11 +59,6 @@ $(document).ready(function() {
         $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
       });
     });
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input.new-street").val("");
-    $("input.new-city").val("");
-    $("input.new-state").val("");
+    resetFields();
   });
 });
